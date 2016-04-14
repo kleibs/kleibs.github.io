d88a156410a0715361221ee7e44e153452928d52
Michael Kleibscheidel <kleibscheidel@gmail.com>
2016-04-14 16:59:11 -0400

    Hide dropdown menu after selection and fix hamburger color

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Close dropdown nav menu after use 
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+	if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+	    $(this).collapse('hide');
+	}
+});
